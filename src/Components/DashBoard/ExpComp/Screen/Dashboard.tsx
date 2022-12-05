cbadc164dd2495c9b72c34a7ce4aa923d7e36da9
unknown <brighterdayscodelab@gmail.com>
2022-12-05 23:03:56 +0100

    new changes

--- src/Components/DashBoard/ExpComp/Screen/Dashboard.tsx
+++ src/Components/DashBoard/ExpComp/Screen/Dashboard.tsx
@@ -2,288 +2,285 @@ import React from "react";
 import styled from "styled-components";
 import ChartComp from "./ChartComp";
 import RecentComp from "./RecentComp";
-import { FaDollarSign, FaFilePdf } from "react-icons/fa";
-import { MdGroup, MdInventory } from "react-icons/md";
 
 const Dashboard: React.FC = () => {
-	return (
-		<div>
-			<Container>
-				<Main>
-					<Wrapper>
-						<Top>
-							<Box bx='gold'>
-								<Boxhold>
-									<Ups>
-										<Texxt>
-											<H1>Income</H1>
-											<Number>#94,0000</Number>
-										</Texxt>
-									</Ups>
+  return (
+    <div>
+      <Container>
+        <Main>
+          <Wrapper>
+            <Top>
+              <Box bx="gold">
+                <Boxhold>
+                  <Ups>
+                    <Texxt>
+                      <H1>Income</H1>
+                      <Number>#94,0000</Number>
+                    </Texxt>
+                  </Ups>
 
-									<Down>
-										<h1 style={{ color: "gold" }}>3.7%</h1> since last week
-									</Down>
-								</Boxhold>
-							</Box>
-							<Box bx='skyblue'>
-								<Boxhold>
-									<Ups>
-										<Texxt>
-											<H1>Expense</H1>
-											<Number>#94,0000</Number>
-										</Texxt>
-									</Ups>
+                  <Down>
+                    <h1 style={{ color: "gold" }}>3.7%</h1> since last week
+                  </Down>
+                </Boxhold>
+              </Box>
+              <Box bx="skyblue">
+                <Boxhold>
+                  <Ups>
+                    <Texxt>
+                      <H1>Expense</H1>
+                      <Number>#94,0000</Number>
+                    </Texxt>
+                  </Ups>
 
-									<Down>
-										<h1>3.7%</h1> since last week
-									</Down>
-								</Boxhold>
-							</Box>
-							<Box bx='purple'>
-								<Boxhold>
-									<Upd>
-										<Texxt>
-											<H1>Sales</H1>
-											<Number>#95,0000</Number>
-										</Texxt>
-									</Upd>
+                  <Down>
+                    <h1>3.7%</h1> since last week
+                  </Down>
+                </Boxhold>
+              </Box>
+              <Box bx="purple">
+                <Boxhold>
+                  <Upd>
+                    <Texxt>
+                      <H1>Sales</H1>
+                      <Number>#95,0000</Number>
+                    </Texxt>
+                  </Upd>
 
-									<Down>
-										<h2>4.5%</h2> since last week
-									</Down>
-								</Boxhold>
-							</Box>
-							<Box bx='red'>
-								<Boxhold>
-									<Upg>
-										<Texxt>
-											<H1>Lost</H1>
-											<Number>#78,0000</Number>
-										</Texxt>
-									</Upg>
+                  <Down>
+                    <h2>4.5%</h2> since last week
+                  </Down>
+                </Boxhold>
+              </Box>
+              <Box bx="red">
+                <Boxhold>
+                  <Upg>
+                    <Texxt>
+                      <H1>Lost</H1>
+                      <Number>#78,0000</Number>
+                    </Texxt>
+                  </Upg>
 
-									<Down>
-										<h3>2.5%</h3> since last week
-									</Down>
-								</Boxhold>
-							</Box>
-						</Top>
-						<Middle>
-							<ChartComp />
-						</Middle>
-						<Middle>
-							<RecentComp />
-						</Middle>
-					</Wrapper>
-				</Main>
-			</Container>
-		</div>
-	);
+                  <Down>
+                    <h3>2.5%</h3> since last week
+                  </Down>
+                </Boxhold>
+              </Box>
+            </Top>
+            <Middle>
+              <ChartComp />
+            </Middle>
+            <Middle>
+              <RecentComp />
+            </Middle>
+          </Wrapper>
+        </Main>
+      </Container>
+    </div>
+  );
 };
 
 export default Dashboard;
 // const Container = styled.div``
 
 const Top = styled.div`
-	width: 100%;
-	min-height: 150px;
-	/* background-color: gold; */
+  width: 100%;
+  min-height: 150px;
+  /* background-color: gold; */
 
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	/* flex-direction: row; */
-	flex-wrap: wrap;
-	margin-top: 41px;
-	/* background-color: blue; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* flex-direction: row; */
+  flex-wrap: wrap;
+  margin-top: 41px;
+  /* background-color: blue; */
 `;
 const Box = styled.div<{ bx: string }>`
-	width: 230px;
-	height: 100px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	border-radius: 10px;
-	background-color: rgb(255, 255, 255);
-	margin: 5px;
-	border-bottom: 2px solid ${(props) => props.bx};
+  width: 230px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  background-color: rgb(255, 255, 255);
+  margin: 5px;
+  border-bottom: 2px solid ${(props) => props.bx};
 
-	@media (max-width: 1245px) {
-		width: 45%;
-		margin: 7px;
-	}
-	@media (max-width: 425px) {
-		width: 95%;
-		margin: 7px;
-	}
+  @media (max-width: 1245px) {
+    width: 45%;
+    margin: 7px;
+  }
+  @media (max-width: 425px) {
+    width: 95%;
+    margin: 7px;
+  }
 `;
 const Boxhold = styled.div`
-	width: 85%;
-	height: 80%;
-	display: flex;
-	justify-content: space-between;
-	flex-direction: column;
+  width: 85%;
+  height: 80%;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
 `;
 const Up = styled.div`
-	width: 85%;
-	height: 55px;
-	display: flex;
-	justify-content: space-between;
-	flex-direction: row;
-	align-items: center;
+  width: 85%;
+  height: 55px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
 `;
 const Ups = styled.div`
-	width: 75%;
-	height: 60px;
-	display: flex;
-	justify-content: space-between;
-	flex-direction: row;
-	align-items: center;
+  width: 75%;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
 `;
 const Upd = styled.div`
-	width: 68%;
-	height: 60px;
-	display: flex;
-	justify-content: space-between;
-	flex-direction: row;
-	align-items: center;
+  width: 68%;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
 `;
 const Upg = styled.div`
-	width: 70%;
-	height: 60px;
-	display: flex;
-	justify-content: space-between;
-	flex-direction: row;
-	align-items: center;
+  width: 70%;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
 `;
 
 const Texxt = styled.div`
-	height: 80%;
-	display: flex;
-	justify-content: space-between;
-	flex-direction: column;
+  height: 80%;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
 `;
 const H1 = styled.div`
-	font-size: 16px;
-	font-weight: bold;
+  font-size: 16px;
+  font-weight: bold;
 `;
 const Number = styled.div`
-	font-size: 16px;
+  font-size: 16px;
 `;
 const Mid = styled.div`
-	width: 100%;
-	height: 5px;
-	display: flex;
-	justify-content: space-between;
-	flex-direction: row;
-	border-radius: 10px;
+  width: 100%;
+  height: 5px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  border-radius: 10px;
 `;
 const Skyblue = styled.div`
-	background-color: skyblue;
-	width: 80%;
-	height: 100%;
+  background-color: skyblue;
+  width: 80%;
+  height: 100%;
 `;
 const Red = styled.div`
-	background-color: red;
-	width: 50%;
-	height: 100%;
+  background-color: red;
+  width: 50%;
+  height: 100%;
 `;
 const Purple = styled.div`
-	width: 30%;
-	background-color: purple;
-	height: 100%;
+  width: 30%;
+  background-color: purple;
+  height: 100%;
 `;
 const Down = styled.div`
-	color: black;
-	font-size: 13px;
-	display: flex;
-	justify-content: space-between;
-	flex-direction: row;
-	align-items: center;
-	width: 70%;
+  color: black;
+  font-size: 13px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  width: 70%;
 
-	span {
-		color: yellow;
-		font-size: 13px;
-		font-weight: bold;
-	}
-	h1 {
-		color: skyblue;
-		font-size: 13px;
-		font-weight: bold;
-	}
-	h2 {
-		color: red;
-		font-size: 13px;
-		font-weight: bold;
-	}
-	h3 {
-		color: purple;
-		font-size: 13px;
-		font-weight: bold;
-	}
+  span {
+    color: yellow;
+    font-size: 13px;
+    font-weight: bold;
+  }
+  h1 {
+    color: skyblue;
+    font-size: 13px;
+    font-weight: bold;
+  }
+  h2 {
+    color: red;
+    font-size: 13px;
+    font-weight: bold;
+  }
+  h3 {
+    color: purple;
+    font-size: 13px;
+    font-weight: bold;
+  }
 `;
 const Middle = styled.div`
-	display: flex;
-
-	justify-content: center;
+  display: flex;
+  justify-content: center;
 `;
 
 const Wrapper = styled.div`
-	width: 90%;
-	/* min-height: 100%; */
-	/* background-color: red; */
-	display: flex;
-	justify-content: center;
-	flex-direction: column;
-	margin-top: 20px;
-	/* margin-left: 180px; */
+  width: 90%;
+  /* min-height: 100%; */
+  /* background-color: red; */
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 20px;
+  /* margin-left: 180px; */
 
-	@media (max-width: 1100px) {
-		margin-left: 200px;
-	}
-	@media (max-width: 1000px) {
-		margin-left: 0px;
-		width: 98%;
-	}
+  @media (max-width: 1100px) {
+    margin-left: 200px;
+  }
+  @media (max-width: 1000px) {
+    margin-left: 0px;
+    width: 98%;
+  }
 `;
 
 const Container = styled.div`
-	/* width: 100%; */
-	width: calc(100vw - 190px);
-	min-height: calc(100vh - 60px);
-	display: flex;
-	justify-content: center;
-	align-items: flex-end;
-	background-color: #f8f9fa;
-	/* background-color: gold; */
-	overflow: hidden;
-	position: absolute;
-	right: 0px;
-	top: 50px;
+  /* width: 100%; */
+  width: calc(100vw - 190px);
+  min-height: calc(100vh - 60px);
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  background-color: #f8f9fa;
+  /* background-color: gold; */
+  overflow: hidden;
+  position: absolute;
+  right: 0px;
+  top: 50px;
 
-	@media screen and (max-width: 1100px) {
-		width: 95%;
-	}
-	@media screen and (max-width: 1005px) {
-		width: 100%;
-	}
+  @media screen and (max-width: 1100px) {
+    width: 95%;
+  }
+  @media screen and (max-width: 1005px) {
+    width: 100%;
+  }
 
-	/* background-color: #352b1e; */
+  /* background-color: #352b1e; */
 `;
 
 const Main = styled.div`
-	/* background-color: blue; */
-	width: 100%;
-	/* height: 92%; */
+  /* background-color: blue; */
+  width: 100%;
+  /* height: 92%; */
 
-	min-height: calc(100vh - 60px);
-	display: flex;
-	justify-content: center;
-	/* align-items: center; */
+  min-height: calc(100vh - 60px);
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
 
-	@media (max-width: 768px) {
-		width: 100%;
-	}
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
--- src/Components/DashBoard/ExpComp/Screen/RecentComp.tsx
+++ src/Components/DashBoard/ExpComp/Screen/RecentComp.tsx
@@ -12,211 +12,245 @@ import numeral from "numeral";
 const url = "https://event-3p90.onrender.com";
 
 interface iData {
-	_id: string;
-	submittedBy: string;
-	image: string;
-	totalExpense: number;
-	totalSales: number;
-	hubName: string;
-	date: string;
-	profit: number;
+  _id: string;
+  submittedBy: string;
+  image: string;
+  totalExpense: number;
+  totalSales: number;
+  hubName: string;
+  date: string;
+  profit: number;
+  map(): () => {};
+  // flat(): void
 }
 
 interface iData2 {
-	_id?: string;
-	name?: string;
-	staff?: string;
-	createdAt?: string;
-	updatedAt?: string;
-	hubToken?: string;
+  _id?: string;
+  name?: string;
+  staff?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  hubToken?: string;
 }
 
 const RecentComp = () => {
-	const [sales, setSales] = useState([] as iData[]);
-	const user = useRecoilValue(userDecode);
-	const [staffData, setStaffData] = useState({} as iData2);
+  const sortedSales = (props: any) => {
+    return (a: any, b: any) => {
+      if (a[props] > b[props]) {
+        return -1;
+      } else {
+        return 0;
+      }
+    };
+  };
 
-	const getAll = async () => {
-		const newURL = `${url}/api/sales/${user._id}/record`;
-		await axios.patch(newURL).then((res) => {
-			setSales(res.data.data.salesRecord);
-			// console.log("this is the salertgfd", sales);
-		});
-	};
+  const groupBy = (props: any, items: any) => {
+    return props.reduce((data: any, item: any) => {
+      (data[item[items]] = data[item[items]] || []).push(item);
+      return data;
+    }, {});
+  };
 
-	const getStaff = async () => {
-		const newURL = `${url}/api/hub/${user._id}/hubstaff`;
-		await axios.get(newURL).then((res) => {
-			setStaffData(res.data.data);
-			console.log("getting all ", staffData);
-		});
-	};
-	useEffect(() => {
-		getAll();
-		getStaff();
-	}, []);
+  const [sales, setSales] = useState([] as iData[]);
+  const [salesDated, setSalesDated] = useState([] as any[]);
 
-	return (
-		<Container>
-			<h3>Staff Leaderboard</h3>
-			<Buttom>
-				<Head>
-					<Th>
-						<HoldHead>
-							Staff
-							<span>
-								<FaLongArrowAltUp color='lightgray' />
-								<FaLongArrowAltDown color='lightgray' />
-							</span>
-						</HoldHead>
-					</Th>
-					<Th>
-						<HoldHead>
-							Registered Date{" "}
-							<span>
-								<FaLongArrowAltUp color='lightgray' />
-								<FaLongArrowAltDown color='lightgray' />
-							</span>
-						</HoldHead>
-					</Th>
+  const user = useRecoilValue(userDecode);
+  const [staffData, setStaffData] = useState({} as iData2);
 
-					<Th>
-						<HoldHead>
-							Profit{" "}
-							<span>
-								<FaLongArrowAltUp color='lightgray' />
-								<FaLongArrowAltDown color='lightgray' />
-							</span>
-						</HoldHead>
-					</Th>
-					<Th>
-						<HoldHead>
-							Total Sales{" "}
-							<span>
-								<FaLongArrowAltUp color='lightgray' />
-								<FaLongArrowAltDown color='lightgray' />
-							</span>
-						</HoldHead>
-					</Th>
-					<Th>
-						<HoldHead>
-							Total Expense{" "}
-							<span>
-								<FaLongArrowAltUp color='lightgray' />
-								<FaLongArrowAltDown color='lightgray' />
-							</span>
-						</HoldHead>
-					</Th>
-				</Head>
+  const getAll = async () => {
+    const newURL = `${url}/api/sales/${user._id}/record`;
+    await axios.patch(newURL).then((res) => {
+      setSales(res.data.data.salesRecord.sort(sortedSales("profit")));
+    });
+  };
 
-				{sales?.map((props) => (
-					<Body key={props._id}>
-						<Td>
-							<UserHold>
-								<span>
-									<Image src={props.image} />
-								</span>
-								{props.submittedBy}
-							</UserHold>
-						</Td>
-						<Td>{props.date}</Td>
+  const getAllDated = async () => {
+    let data = Object.values(groupBy(sales, "dated")).flat();
+    // setSalesDated(Object.values(groupBy(sales, "dated")));
+    setSalesDated(
+      data
+        .map((el: any) => el.profit)
+        .reduce((a: number, b: number) => {
+          return a + b;
+        })
+    );
+  };
 
-						<Td> ₦ {numeral(props.profit).format("0,0")} </Td>
-						<Td> ₦{numeral(props.totalSales).format("0,0")} </Td>
-						<Td> ₦{numeral(props.totalExpense).format("0,0")} </Td>
-					</Body>
-				))}
-			</Buttom>
-		</Container>
-	);
+  const getStaff = async () => {
+    const newURL = `${url}/api/hub/${user._id}/hubstaff`;
+    await axios.get(newURL).then((res) => {
+      setStaffData(res.data.data);
+      console.log("getting all ", staffData);
+    });
+  };
+
+  useEffect(() => {
+    getAll();
+    getStaff();
+    getAllDated();
+  }, []);
+
+  return (
+    <Container>
+      <h3>Staff Leading Sales Record</h3>
+      <Buttom>
+        <Head>
+          <Th>
+            <HoldHead>
+              Staff
+              <span>
+                <FaLongArrowAltUp color="lightgray" />
+                <FaLongArrowAltDown color="lightgray" />
+              </span>
+            </HoldHead>
+          </Th>
+          <Th>
+            <HoldHead>
+              Registered Date{" "}
+              <span>
+                <FaLongArrowAltUp color="lightgray" />
+                <FaLongArrowAltDown color="lightgray" />
+              </span>
+            </HoldHead>
+          </Th>
+
+          <Th>
+            <HoldHead>
+              Profit{" "}
+              <span>
+                <FaLongArrowAltUp color="lightgray" />
+                <FaLongArrowAltDown color="lightgray" />
+              </span>
+            </HoldHead>
+          </Th>
+          <Th>
+            <HoldHead>
+              Total Sales{" "}
+              <span>
+                <FaLongArrowAltUp color="lightgray" />
+                <FaLongArrowAltDown color="lightgray" />
+              </span>
+            </HoldHead>
+          </Th>
+          <Th>
+            <HoldHead>
+              Total Expense{" "}
+              <span>
+                <FaLongArrowAltUp color="lightgray" />
+                <FaLongArrowAltDown color="lightgray" />
+              </span>
+            </HoldHead>
+          </Th>
+        </Head>
+
+        {sales?.map((props) => (
+          <Body key={props._id}>
+            <Td>
+              <UserHold>
+                <span>
+                  <Image src={props.image} />
+                </span>
+                {props.submittedBy}
+              </UserHold>
+            </Td>
+            <Td>{props.date}</Td>
+
+            <Td> ₦ {numeral(props.profit).format("0,0")} </Td>
+            <Td> ₦{numeral(props.totalSales).format("0,0")} </Td>
+            <Td> ₦{numeral(props.totalExpense).format("0,0")} </Td>
+          </Body>
+        ))}
+      </Buttom>
+    </Container>
+  );
 };
 
 export default RecentComp;
 
 const Container = styled.div`
-	width: 90%;
-	overflow-x: scroll;
-	scroll-behavior: smooth;
-	scroll-snap-type: x mandatory;
-	background-color: white;
+  width: 90%;
+  overflow-x: scroll;
+  scroll-behavior: smooth;
+  scroll-snap-type: x mandatory;
+  background-color: white;
 
-	@media screen and (max-width: 700px) {
-		width: 94%;
-	}
+  @media screen and (max-width: 700px) {
+    width: 94%;
+  }
 `;
 const Image = styled.img`
-	width: 40px;
-	height: 40px;
-	border-radius: 50%;
-	background-color: gold;
-	margin-right: 10px;
-	object-fit: cover;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: gold;
+  margin-right: 10px;
+  object-fit: cover;
 `;
 const Button = styled.button<{ bd: string; cl: string }>`
-	padding: 7px 20px;
-	background-color: #ece5e5;
-	border: ${({ bd }) => (bd ? "1px solid gray" : "0px")};
-	border-radius: 5px;
-	margin: 5px;
-	color: ${({ cl }) => (cl ? "#109f10" : "red")};
-	font-weight: 600;
-	cursor: pointer;
-	font-size: 12px;
-	font-family: Poppins;
-	span {
-		margin-left: 3px;
-	}
+  padding: 7px 20px;
+  background-color: #ece5e5;
+  border: ${({ bd }) => (bd ? "1px solid gray" : "0px")};
+  border-radius: 5px;
+  margin: 5px;
+  color: ${({ cl }) => (cl ? "#109f10" : "red")};
+  font-weight: 600;
+  cursor: pointer;
+  font-size: 12px;
+  font-family: Poppins;
+  span {
+    margin-left: 3px;
+  }
 `;
 const Hold = styled.div``;
 const HoldHead = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 const UserHold = styled.div`
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
 
-	:hover {
-		color: blue;
-	}
-	cursor: pointer;
+  :hover {
+    color: blue;
+  }
+  cursor: pointer;
 `;
 const Td = styled.td`
-	padding: 10px 15px;
-	font-size: 12px;
-	font-weight: 400;
-	width: 250px;
-	border-left: 1px solid;
-	border-right: 1px solid;
-	border-color: rgba(0, 0, 0, 0.1);
+  padding: 10px 15px;
+  font-size: 12px;
+  font-weight: 400;
+  width: 250px;
+  border-left: 1px solid;
+  border-right: 1px solid;
+  border-color: rgba(0, 0, 0, 0.1);
 `;
 const Th = styled.th`
-	padding: 10px 10px;
-	background-color: #f8f9fa;
-	width: 250px;
-	/* #f8f9fa */
-	color: #1b2559;
-	font-size: 13px;
-	font-weight: 600;
-	text-align: start;
-	// border: 1px solid black;
+  padding: 10px 10px;
+  background-color: #f8f9fa;
+  width: 250px;
+  /* #f8f9fa */
+  color: #1b2559;
+  font-size: 13px;
+  font-weight: 600;
+  text-align: start;
+  // border: 1px solid black;
 `;
 const Head = styled.tr`
-	border-bottom: 1px solid lightgray;
-	background-color: white;
+  border-bottom: 1px solid lightgray;
+  background-color: white;
 `;
 const Body = styled.tr`
-	border-bottom: 1px solid lightgray;
-	:hover {
-		background-color: lightgray;
-	}
+  border-bottom: 1px solid lightgray;
+  :hover {
+    background-color: lightgray;
+  }
 `;
 const Buttom = styled.table`
-	width: 78rem;
-	/* padding: 0px 20px; */
+  width: 78rem;
+  /* padding: 0px 20px; */
 
-	text-align: center;
-	border-collapse: collapse;
-	border-spacing: 0;
+  text-align: center;
+  border-collapse: collapse;
+  border-spacing: 0;
 `;
