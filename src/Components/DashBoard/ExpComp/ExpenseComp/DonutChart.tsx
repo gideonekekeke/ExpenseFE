a4080dfb25a527f1e098284a426b10926e8849d3
gideonekekeke <gideonekeke64@gmail.com>
2022-12-08 20:27:55 +0100

    myown

--- src/Components/DashBoard/ExpComp/ExpenseComp/DonutChart.tsx
+++ src/Components/DashBoard/ExpComp/ExpenseComp/DonutChart.tsx
@@ -1,35 +1,89 @@
+import axios from "axios";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { useEffect, useState } from "react";
 import { Doughnut } from "react-chartjs-2";
+import { useRecoilValue } from "recoil";
+import { userDecode } from "../../../Global/GlobalState";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export const data = {
-	labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-	datasets: [
-		{
-			label: "# of Votes",
-			data: [12, 19, 3, 5, 2, 3],
-			backgroundColor: [
-				"rgba(255, 99, 132, 0.2)",
-				"rgba(54, 162, 235, 0.2)",
-				"rgba(255, 206, 86, 0.2)",
-				"rgba(75, 192, 192, 0.2)",
-				"rgba(153, 102, 255, 0.2)",
-				"rgba(255, 159, 64, 0.2)",
-			],
-			borderColor: [
-				"rgba(255, 99, 132, 1)",
-				"rgba(54, 162, 235, 1)",
-				"rgba(255, 206, 86, 1)",
-				"rgba(75, 192, 192, 1)",
-				"rgba(153, 102, 255, 1)",
-				"rgba(255, 159, 64, 1)",
-			],
-			borderWidth: 1,
-		},
-	],
-};
+const url = "https://event-3p90.onrender.com";
+
+interface iData {
+	_id: string;
+	submittedBy: string;
+	image: string;
+	totalExpense: number;
+	totalSales: number;
+	hubName: string;
+	date: string;
+	profit: number;
+	note: string;
+}
+interface iData2 {
+	_id: string;
+}
+
 const DonutChart = () => {
+	const user = useRecoilValue(userDecode);
+	const [sales, setSales] = useState([] as iData[]);
+	const [prof, setProf] = useState<number>(0);
+
+	const getAll = async () => {
+		const newURL = `${url}/api/sales/${user._id}/record`;
+		await axios.patch(newURL).then((res) => {
+			setSales(res.data.data.salesRecord);
+			// console.log("this is   ", sales);
+		});
+	};
+
+	let hold: number[] = [];
+	const getProfits = () => {
+		setProf(
+			sales
+				.map((el) => {
+					return el.profit;
+				})
+				.reduce((a: number, b: number) => {
+					return a + b;
+				}, 0),
+		);
+	};
+
+	console.log("this is dfgdg gfdgdvdf", prof);
+
+	useEffect(() => {
+		getAll();
+		getProfits();
+	}, []);
+
+	const data = {
+		labels: ["Sales", "Expenses", "Profit"],
+		datasets: [
+			{
+				label: "# of Votes",
+				data: [12, 19, 3],
+				backgroundColor: [
+					"rgba(255, 99, 132, 0.2)",
+					"rgba(54, 162, 235, 0.2)",
+					"rgba(255, 206, 86, 0.2)",
+					"rgba(75, 192, 192, 0.2)",
+					"rgba(153, 102, 255, 0.2)",
+					"rgba(255, 159, 64, 0.2)",
+				],
+				borderColor: [
+					"rgba(255, 99, 132, 1)",
+					"rgba(54, 162, 235, 1)",
+					"rgba(255, 206, 86, 1)",
+					"rgba(75, 192, 192, 1)",
+					"rgba(153, 102, 255, 1)",
+					"rgba(255, 159, 64, 1)",
+				],
+				borderWidth: 1,
+			},
+		],
+	};
+
 	return (
 		<div
 			style={{
